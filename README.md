b0a5fe10f9e2a5ea674b4c99146f9f5b5257a086
Liam Axelrod <liamaxelrod@gmail.com>
2021-11-17 21:04:06 +0100

    copied from love

--- README.md
+++
@@ -1,7 +0,0 @@
-# JCLAPEL Banking System
-
-Group 7's project for DIT094
-
-## TODO
-- List simple tasks for the repository here, not specific work tasks
-- (For MacOS users) If you are having trouble with the passphrased SSH((i.e. unable to connect to the repository), you should try using `ssh-add` at least once because you need the SSH Agent.
---
+++ src/main/java/com/example/jc_lapel/HelloController.java
@@ -0,0 +1,59 @@
+package com.example.jc_lapel;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void switchToLoginCustomer(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("loginCustomer.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToHelloView(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToRegisterCustomer(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("registerCustomer.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    //Hej Erik... Update jaooow
+    @FXML
+    public Button closeButton;
+
+    @FXML
+    public void handelCloseButtonAction(ActionEvent event){
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
+}
