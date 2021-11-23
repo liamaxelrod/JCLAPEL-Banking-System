1719cd31c05eee70c8c4628037363dbc5399e54b
albinkar <albinkar@student.chalmers.se>
2021-11-23 19:05:04 +0100

    Checked if login works with specific code

--- src/main/java/com/example/BackEnd/createAccount.java
+++ src/main/java/com/example/BackEnd/createAccount.java
@@ -1,5 +1,53 @@
 package com.example.BackEnd;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+
 public class createAccount {
-    //liam Axelrod
+
+    static ArrayList<createAccount> listAccounts = new ArrayList<createAccount>(); // creating the list of accounts
+
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private PasswordField confirmPasswordField;
+
+    @FXML
+    private Button createAccountButton;
+
+    @FXML
+    private TextField firstnameTextField;
+
+    @FXML
+    private TextField lastnameTextField;
+
+    @FXML
+    private PasswordField setPasswordField;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    void onActionCreateAccount(ActionEvent event) {
+
+    }
+
+    public void registerUser(){
+        String firstname = firstnameTextField.getText();
+        String lastname = lastnameTextField.getText();
+        String username = usernameTextField.getText();
+        String password = setPasswordField.getText();
+
+        String insertFields = "";
+        String insertValues = "";
+        String insertToRegister = insertFields + insertValues;
+    }
+
 }
