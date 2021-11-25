81a0614fc568e7e06b1c43b5220e7e02c88fb341
albinkar <albinkar@student.chalmers.se>
2021-11-25 10:13:38 +0100

    Master main

--- src/main/java/com/example/FrontEnd/LoginController.java
+++ src/main/java/com/example/FrontEnd/LoginController.java
@@ -61,7 +61,7 @@ public class LoginController implements Initializable {
 
     @FXML//on interface button = login
     public void switchToCustomerMenu(ActionEvent event) throws IOException{
-        if (usernameTextField.getText().toString().equals("Albin") && enterPasswordField.getText().toString().equals("123")){
+        if (usernameTextField.getText().toString().equals("Love") && enterPasswordField.getText().toString().equals("Love")){
             Parent root = FXMLLoader.load(getClass().getResource("userMenu.fxml"));
             stage = (Stage)((Node)event.getSource()).getScene().getWindow();
             scene = new Scene(root);
