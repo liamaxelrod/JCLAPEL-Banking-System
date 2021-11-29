4fd14dfdb9d33796b3124b05b1779c9f00923857
connylu <connylu@student.chalmers.se>
2021-11-29 18:18:58 +0100

    Modified spaces to tabs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,14 @@
 module com.example.jc_lapel {
 
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires java.sql;
 
-    opens com.example.FrontEnd to javafx.fxml;
-    exports com.example.FrontEnd;
+	opens com.example.FrontEnd to javafx.fxml;
+	exports com.example.FrontEnd;
 
-    opens com.jclapel.banksystem to javafx.fxml;
-    exports com.jclapel.banksystem.data;
-    exports com.jclapel.banksystem.facade;
-    exports com.jclapel.banksystem.frontend;
+	opens com.jclapel.banksystem to javafx.fxml;
+	exports com.jclapel.banksystem.data;
+	exports com.jclapel.banksystem.facade;
+	exports com.jclapel.banksystem.frontend;
 }
