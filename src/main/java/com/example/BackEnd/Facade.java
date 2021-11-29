1e54cd4c4016fd96b120180c132f397a0f293020
nihiliss <samcenko@student.chalmers.se>
2021-11-29 08:30:19 +0100

    Added facade and skeleton of the functionalities to be added by others from their unsynchronised code

---
+++ src/main/java/com/example/BackEnd/Facade.java
@@ -0,0 +1,53 @@
+package com.example.BackEnd;
+
+import java.util.ArrayList;
+
+public class Facade {
+    private ArrayList<Customer> customers = new ArrayList<Customer>();
+
+    public ArrayList<Customer> loadCustomers() {
+        //method to load the customers from external storage upon starting the application
+        return new ArrayList<>();
+    }
+
+    public void storeCustomers() {
+        //method to store customers to external storage before closing the app
+    }
+
+    public void createCustomer(){
+        //method for creating a new customer
+    }
+
+    public void removeCustomer(){
+        //method for removing a customer
+    }
+
+    public boolean checkLogin(Customer customer, String userName, String password){
+        //check the credentials of a user trying to log in
+        return true;
+    }
+
+    public void addTransfer(){
+        //create a new transfer
+    }
+
+    public void createAccount(){
+        //create a new account
+    }
+
+    public void findCustomer(){
+        //locate a single customer
+    }
+
+    public void findAccount(){
+        //locate a single account
+    }
+
+    public void findTransaction(){
+        //locate a single transaction
+    }
+
+    public void loadAllTransactions(){
+        //load all transactions for an account
+    }
+}
