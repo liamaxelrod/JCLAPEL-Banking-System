90a32c1604aead602f564065b0aba45eb5b810d7
nihiliss <samcenko@student.chalmers.se>
2021-12-02 13:07:22 +0100

    added transaction

--- src/main/java/com/example/BackEnd/Transaction.java
+++ src/main/java/com/example/BackEnd/Transaction.java
@@ -1,4 +1,24 @@
 package com.example.BackEnd;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Transaction {
+    private double amount;
+    private LocalDate date;
+    private LocalTime time;
+
+    public Transaction(double amount){
+        this.amount=amount;
+        date = LocalDate.now();
+        time=LocalTime.now();
+
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+
+
+
 }
