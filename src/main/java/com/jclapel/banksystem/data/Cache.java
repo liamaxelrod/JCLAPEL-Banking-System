e42233ba02f91e618f094f053bd33dcc59dfa227
connylu <connylu@student.chalmers.se>
2021-12-06 14:22:15 +0100

    Updated Cache.java to latest iteration

--- src/main/java/com/jclapel/banksystem/data/Cache.java
+++ src/main/java/com/jclapel/banksystem/data/Cache.java
@@ -6,34 +6,64 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Set;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class Cache implements Serializable {
 	/*
 
 	Cache
-	A class to manage data and streams between the system and an assumed existing database.
+	A class to manage data and streams between the system and an assumed existing database. 
+	This will serialize Java objects into JSON objects and upload the JSON objects into a database(temporarily will be locally stored).
+	Also will deserialize JSON objects after requesting from the same database.
 
 	*/
+	private final boolean USE_LOCAL_STORAGE = true;
+	private final boolean USE_DATABASE = false;
+
+	private FileInputStream cacheSource;
+	private FileOutputStream cacheTarget;
+
 	private HashMap<String, Object> dataCache;
 	
 	// Gson gson = new Gson();
 
-	public void initialize() {
-		// Executes initial procedure on program start
-		dataCache = new HashMap<String, Object>();
-	}
-
-	public void initialize(Set<String> keySet) {
-		// Executes initial procedure on program start
-		dataCache = new HashMap<String, Object>();
-
-		for (String key : keySet) {
-			dataCache.put(key, "");
+	private void setupLocalStorage() throws Exception {
+		try {
+			cacheSource = new FileInputStream("data.json");
+			cacheTarget = new FileOutputStream("data.json");
+		} catch(Exception e) {
+			// Error handling here
+		} finally {
+			if (cacheSource != null) {
+				cacheSource.close();
+			}
+			
+			if (cacheTarget != null) {
+				cacheTarget.close();
+			}
 		}
 	}
 
-	public void initialize(HashMap<String, Object> presetCache) {
+	public void initialize() throws Exception {
+		// Executes initial procedure on program start
+		dataCache = new HashMap<String, Object>();
+
+		setupLocalStorage();
+	}
+
+	public void initialize(Set<String> keySet) throws Exception {
+		// Executes initial procedure on program start
+		dataCache = new HashMap<String, Object>();
+
+		setupLocalStorage();
+	}
+
+	public void initialize(HashMap<String, Object> presetCache) throws Exception {
 		// Executes initial procedure on program start
 		dataCache = presetCache;
+
+		setupLocalStorage();
 	}
 
 	public void appendData(String key, Object object) {
@@ -64,13 +94,27 @@ public class Cache implements Serializable {
 		// TODO: Send data with this string
 		//String serializedData = gson.toJson(dataCache);
 
+		if (USE_LOCAL_STORAGE) {
+			// Save data locally
+		}
+
+		if (USE_DATABASE) {
+			// Save data on database
+		}
+
 		//System.out.println(serializedData); // Needs further testing
 	}
 
 	public void loadData(String serializedData) {
 		// Deserializes inbound data acquired from database
 		// TODO: Convert the string into a map
-		
+		if (USE_LOCAL_STORAGE) {
+			// Get data locally
+		}
+
+		if (USE_DATABASE) {
+			// Get data on database
+		}
 	}
 
 	public void close() {
