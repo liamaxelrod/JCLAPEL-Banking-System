2b236cc4a3670bcf3daa17c856ed0f52483bbee5
connylu <connylu@student.chalmers.se>
2021-12-06 15:27:58 +0100

    Added TransferTransaction

---
+++ src/main/java/com/jclapel/banksystem/back_end/TransferTransaction.java
@@ -0,0 +1,16 @@
+package com.jclapel.banksystem.back_end;
+
+import java.time.LocalDate;
+
+public class TransferTransaction extends Transaction {
+    private int receiverID;
+	 
+    public TransferTransaction(double amount, int accountId, int receiverID) {
+        super(amount, accountId);
+        this.receiverID = receiverID;
+    }
+
+    public int getReceiverID() {
+        return receiverID;
+    }
+}
