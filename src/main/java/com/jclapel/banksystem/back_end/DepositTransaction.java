a56f857b02dd31dea713de40803a8f4feee716e4
connylu <connylu@student.chalmers.se>
2021-12-06 15:17:11 +0100

    Added DepositTransaction from main

---
+++ src/main/java/com/jclapel/banksystem/back_end/DepositTransaction.java
@@ -0,0 +1,7 @@
+package com.jclapel.banksystem.back_end;
+
+public class DepositTransaction extends Transaction {
+    public DepositTransaction(double amount, int accountId) {
+        super(amount, accountId);
+    }
+}
