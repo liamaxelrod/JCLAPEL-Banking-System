e71951d130a4034213f6dd6e57f4b4d89be83b56
connylu <connylu@student.chalmers.se>
2021-12-06 15:35:41 +0100

    Synchronized Employee

--- src/main/java/com/jclapel/banksystem/back_end/Employee.java
+++ src/main/java/com/jclapel/banksystem/back_end/Employee.java
@@ -1,4 +1,34 @@
 package com.jclapel.banksystem.back_end;
 
 public class Employee {
+	/*
+
+	PLACEHOLDER
+
+	*/
+
+    private final int ID;
+    private String name;
+
+    public Employee(int ID, String name) {
+		// Creates employee from ID and name
+		// TODO: Avoid duplicate ID!
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public int getID() {
+		// Returns the employee's ID
+        return ID;
+    }
+
+    public String getName() {
+		// Returns the employee's name
+        return name;
+    }
+
+    public void setName(String name) {
+		// Sets a new name to the employee's name
+        this.name = name;
+    }
 }
