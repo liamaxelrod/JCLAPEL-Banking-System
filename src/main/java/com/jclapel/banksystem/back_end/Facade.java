49d843046e6db446d52e7ea0bca9e5cc1873a9be
connylu <connylu@student.chalmers.se>
2021-12-06 15:24:59 +0100

    Updated Facade to synchronize to main

--- src/main/java/com/jclapel/banksystem/back_end/Facade.java
+++ src/main/java/com/jclapel/banksystem/back_end/Facade.java
@@ -1,75 +1,183 @@
-package com.jclapel.banksystem.back_end;
+package com.example.BackEnd;
 
-import java.util.ArrayList;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Stack;
 
 public class Facade {
-    private ArrayList<Customer> customerList = new ArrayList<Customer>();
+    public HashMap<Integer, Customer> customers = new HashMap<>();
+    public HashMap<Integer, Account> accounts = new HashMap<>();
+    public HashMap<Integer, Employee> employees=new HashMap<>();
 
-    public ArrayList<Customer> loadCustomers() {
-        //method to load the customers from external storage upon starting the application
-        return new ArrayList<>();
+    final static String customersOutputFilePath = "F:/Serialisation/customers.txt";
+    final static String accountsOutputFilePath = "F:/Serialisation/accounts.txt";
+    final static String employeesOutputFilePath = "F:/Serialisation/employees.txt";
+    File customersFile = new File(customersOutputFilePath); //File to save customer data
+    File accountsFile = new File(accountsOutputFilePath); //File to save account data
+    File employeesFile = new File(employeesOutputFilePath); //File to save employee data
+
+    public void storeData() { //stores data to file when the app is closed.
+        try {
+            FileOutputStream customersOutput = new FileOutputStream(customersFile);
+            FileOutputStream accountsOutput = new FileOutputStream(accountsFile);
+            FileOutputStream employeesOutput = new FileOutputStream(employeesFile);
+
+            ObjectOutputStream customersStream = new ObjectOutputStream(customersOutput); //Allows to store objects into file
+            ObjectOutputStream accountsStream = new ObjectOutputStream(accountsOutput);
+            ObjectOutputStream employeesStream = new ObjectOutputStream(employeesOutput);
+
+            customersStream.writeObject(customers); //Actually storing the data to file
+            accountsStream.writeObject(accounts);
+            employeesStream.writeObject(employees);
+
+            customersStream.close();
+            accountsStream.close();
+            employeesStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    } // Done by Julia Ayvazian, temporary solution until the database works
+
+    public void loadData() { //loads all data from file when the app is opened.
+        try {
+            FileInputStream customersInput = new FileInputStream(customersFile);
+            FileInputStream accountsInput = new FileInputStream(accountsFile);
+            FileInputStream employeesInput = new FileInputStream(employeesFile);
+
+            ObjectInputStream customersStream = new ObjectInputStream(customersInput); //Object input stream allows to read objects from file
+            ObjectInputStream accountsStream = new ObjectInputStream(accountsInput);
+            ObjectInputStream employeesStream = new ObjectInputStream(employeesInput);
+
+            customers = (HashMap<Integer, Customer>) customersStream.readObject(); //Loading the hashmap from the file
+            accounts = (HashMap<Integer, Account>) accountsStream.readObject();
+            employees = (HashMap<Integer, Employee>) employeesStream.readObject();
+
+            customersStream.close();
+            accountsStream.close();
+            employeesStream.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    } // Done by Julia Ayvazian, temporary solution until the database works
+
+    public Customer loadCustomer(int customerId){
+        return customers.get(customerId);
     }
 
-    public void storeCustomers() {
-        //method to store customers to external storage before closing the app
+    public int createCustomer(String name, String password){ //Patrik, Karar , Julia, Erik, returns customer id;
+        int ID = generateId(customers);
+        Customer customer = new Customer(ID, name, password);
+        customers.put(ID, customer);
+        return ID;
+    }//patrik, labi, julia
+
+    public int CheckIfCustomerExists(int ID){
+        if(customers.containsKey(ID)){
+            return ID;
+        }else{
+            return 0;
+        }
     }
 
-    public void createCustomer(){
-        //method for creating a new customer
+    public void removeCustomer(int ID){
+        customers.remove(ID);
     }
 
-    public void removeCustomer(){
-        //method for removing a customer
+    public boolean checkLogin(int ID, String password){
+        if(customers.containsKey(ID)){
+            if(customers.get(ID).getPassword().equals(password)) {
+                //checks if the id exists, and the password of the customer with that id is equal to the input password
+                return true;
+            }
+        }
+        return false;
+    } //patrik, labi, julia
+
+    public int createAccount(int customerId){ // adds an account to a given customer
+        Account account = new Account(generateId(customers.get(customerId).getAccounts()), false);
+        customers.get(customerId).addAccount(account);
+        accounts.put(account.getID(), account);
+        return account.getID();
+    } //patrik, labi
+
+    public int createSavingsAccount(int customerId){
+        Account account = new Account(generateId(customers.get(customerId).getAccounts()), true);
+        customers.get(customerId).addAccount(account);
+        accounts.put(account.getID(), account);
+        return account.getID();
+    } //patrik, labi
+
+    public void removeAccount(int accountID){
+        accounts.remove(accountID);
     }
 
-    public boolean checkLogin(Customer customer, String userName, String password){
-        //check the credentials of a user trying to log in
-        return true;
+    public boolean CheckIfAccountExists(int ID){
+        if(accounts.containsKey(ID)){
+            return true;
+        }else{
+            return false;
+        }
     }
 
-    public void transferBetweenAccounts(){
-        //create a new transfer
+    public boolean transferBetweenAccounts(int senderId, int receiverId, double amount) {
+        if (withdraw(senderId, amount) && deposit(receiverId, amount)) {
+            accounts.get(senderId).addTransaction(new TransferTransaction(amount, senderId, receiverId));
+            accounts.get(receiverId).addTransaction(new TransferTransaction(amount, senderId, receiverId));
+            return true;
+        }
+        return false;
+    } //patrik, labi, julia
+
+    public boolean deposit(int accountID, double amount){ //add amount, return true if the transaction is valid, or false if it is invald
+        if(amount>0){
+            accounts.get(accountID).setBalance(accounts.get(accountID).getBalance() + amount*0.99);
+            accounts.get(accountID).addTransaction(new DepositTransaction(amount, accountID));
+            return true;
+        }
+        return false;
+    } //patrik, labi, julia
+
+    public boolean withdraw(int accountID, double amount) {//subtracts amount from balance, returns true for a valid transaction, false for an invalid one
+        if (amount > 0 && accounts.get(accountID).getBalance() >= amount) {
+            Account account = accounts.get(accountID);
+            account.setBalance(account.getBalance() - amount*1.01);
+            account.addTransaction(new WithdrawalTransaction(amount, accountID));
+            return true;
+        }
+        return false;
+    } //patrik, labi, julia
+
+    public Stack<Transaction> loadAllTransactions(int accountID){
+         return accounts.get(accountID).getTransactions();
     }
 
-    public void deposit(){
-        //deposit to the account
-    }
-
-    public void withdraw(){
-        //withdraw from the account
-    }
-
-    public void createAccount(){
-        //create a new account
-    }
-
-    public void findCustomer(){
-        //locate a single customer
-    }
-
-    public void findAccount(){
-        //locate a single account
-    }
-
-    public void findTransaction(){
-        //locate a single transaction
-    }
-
-    public void loadAllTransactions(){
-        //load all transactions for an account
-    }
-
-    public void resetCredentials(){
-        //reset user credentials
-    }
+    public boolean resetPassword(int customerId, String originalPassword, String newPassword){ //returns a boolean indicating whether the change went through
+        if(checkLogin(customerId, originalPassword)) {
+            Customer customer = customers.get(customerId);
+            customer.setPassword(newPassword);
+            return true;
+        }
+        return false;
+    } // By Julia Ayvazian
 
     public void retrieveUserStatistics(){
         //retrieve data to be displayed by user statistics
     }
 
-    public void createEmployee(){
-        //create employee account
+    public void createEmployee(String name){
+        int ID = generateId(employees);
+        Employee employee = new Employee(ID, name);
+        employees.put(ID, employee);
     }
 
-
+    public int generateId(HashMap hashMap){ //takes the hashmap in which the resulting object will be stored as an argument
+        int ID;
+        Random rn = new Random();
+        do{ //generate random ID number
+            int range = 999999 - 100000 +1; //generate 6 digit random number
+            ID = rn.nextInt(range) + 100000;
+        } while (hashMap.containsKey(ID));//ensure the id is not in use
+        return ID;
+    } //patrik
 }
