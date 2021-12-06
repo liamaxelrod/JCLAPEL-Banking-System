4691b1c16f9d95de6ece35b40b2d5ac59d3f2e5e
connylu <connylu@student.chalmers.se>
2021-12-06 15:31:34 +0100

    Synchronized WithdrawalTransaction

---
+++ src/main/java/com/jclapel/banksystem/back_end/WithdrawalTransaction.java
@@ -0,0 +1,13 @@
+package com.jclapel.banksystem.back_end;
+
+public class WithdrawalTransaction extends Transaction { // By Erik Lindmaa
+    /*
+
+    PLACEHOLDER
+
+    */
+    public WithdrawalTransaction(double amount, int accountId) {
+        // Constructor for withdrawal transaction
+        super(amount, accountId);
+    }
+}
